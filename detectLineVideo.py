503185013100c0bb34eeab7cd7a2b5ba8b33a17a
SHIKHA K <103320160+Shikha23K@users.noreply.github.com>
2022-05-22 13:49:08 +0530

    Update detectLineVideo.py

--- detectLineVideo.py
+++ detectLineVideo.py
@@ -1,6 +1,7 @@
 
 """
- Live Lane-Line Detection Systems built-in Python language is another Data Science project idea for beginners.A human driver receives lane detecting instruction from
+
+Live Lane-Line Detection Systems built-in Python language is another Data Science project idea for beginners.A human driver receives lane detecting instruction from
  lines placed on the road in this project. The lines placed on the roads indicate where the lanes are located for human driving. It also refers to the vehicle’s
  steering direction. This application is crucial for the development of self-driving cars. This application for the Data Science Project is critical for the development
  of self-driving cars.
